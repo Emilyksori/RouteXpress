c4c12bd6443b55ed3e7df91225d68f9057a2a2eb
Emilyksori <emilyhiro2908@gmail.com>
2024-10-31 05:28:28 -0300

    data example

---
+++ data/dados.sql
@@ -0,0 +1,3 @@
+nome,endereço,produto,telefone.email
+Felipe Brito de Lima,Rua Brito,Produto1,(15) 99999-9999,felipe@gmail.com
+Amanda da Silva Soares,Rua Soares,Produto2,(15) 88888-8888,amanda@gmail.com
